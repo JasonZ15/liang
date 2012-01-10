e62fb36b2ed3439089f375a1b417800797fe68cf
JasonZ15 <vines15@gmail.com>
2012-01-10 10:04:07 +0800

    move sidebar to left

--- page.php
+++ page.php
@@ -5,14 +5,14 @@
 <div id="main-content">
 	<div id="main-content-bg">
 		<div id="main-content-bottom-bg" class="clearfix">
+			<?php get_sidebar(); ?>
 			<div id="left-area">
 				<div id="main-products" class="clearfix">
 					<?php get_template_part('loop','page'); ?>
 				</div> <!-- end #main-products -->
 				<?php if (get_option('boutique_show_pagescomments') == 'on') comments_template('', true); ?>
 			</div> <!-- end #left-area -->
-			
-			<?php get_sidebar(); ?>	
+				
 		</div> <!-- end #main-content-bottom-bg -->
 	</div> <!-- end #main-content-bg -->
 </div> <!-- end #main-content -->
